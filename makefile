af6ca73507d5522456340fb7181999ab6902e808
mb268 <mb268@cs-ssh.argo.uwf.edu>
2020-12-02 22:37:06 -0600

    attempt at OMP

--- makefile
+++ makefile
@@ -2,15 +2,19 @@ CC=mpicc
 CFLAG=  -I. -Ofast -fopenmp
 
 EXECUTABLE=passguessmpi
+EXECUTABLE2=passguessmpi_mason
 
-all: $(EXECUTABLE)
+all: $(EXECUTABLE) $(EXECUTABLE2)
 
 passguessmpi:  passguessmpi.c microtime.h
 	$(CC) $(CFLAG) -c microtime.c
 	$(CC) $(CFLAG) -o passguessmpi passguessmpi.c microtime.o
 
+passguessmpi_mason:  passguessmpi_mason.c microtime.h
+	$(CC) $(CFLAG) -c microtime.c
+	$(CC) $(CFLAG) -o passguessmpi_mason passguessmpi_mason.c microtime.o
 clean:
 	rm -f *.o *~ core
 
 realclean:
-	rm -f *.o *~ core $(EXECUTABLE)
+	rm -f *.o *~ core $(EXECUTABLE) $(EXECUTABLE2)
---
+++ passguessmpi_mason.c
@@ -0,0 +1,193 @@
+#include <stdio.h>
+#include <microtime.h>
+#include <string.h>
+#include <stdlib.h>
+#include <mpi.h>
+#include <omp.h>
+
+#define DEBUG 0
+
+char* makeGuess(char *guess, char *known, int length, int* unknown) {
+	int i = *unknown;
+	
+        guess[i]++;
+	
+	return guess;
+}
+
+char checkGuess(char *guess, char *pw, char *known, int length, int* unknown) {
+	char isCorrect = 0;
+	int i = *unknown;
+
+	if(guess[i] == pw[i]) {
+		// we found a matching character
+		known[i] = 1;
+		*unknown += 1;
+		if (i == length) {
+			isCorrect = 1;
+		}
+	}
+	return isCorrect;
+}
+
+char* init(int length) {
+	char* arr;
+	arr = malloc(length * sizeof(char));
+	return arr;
+}
+
+int main(int argc, char **argv) {
+
+	int rank, size, linesPerNode,fileLines;
+
+	MPI_Status status;
+	MPI_Init(&argc,&argv);
+	MPI_Comm_rank(MPI_COMM_WORLD,&rank);
+	MPI_Comm_size(MPI_COMM_WORLD,&size); 
+
+	if(DEBUG) printf("RANK %d\t and SIZE %d\n",rank, size);
+
+	if (rank == 0) {
+		double t, time1, time2,totalTime;;
+		FILE * fp;
+		char * line = NULL;
+  		size_t len = 0;
+		ssize_t read;
+		int dest, c,linesLastNode;
+		fileLines = 0;
+
+
+		fp = fopen("PASSWORDFILE.txt", "r");
+
+		while ( (c=fgetc(fp)) != EOF ) {
+			if ( c != '\n' ) {
+	    			read = getline(&line, &len, fp);
+				fileLines++;
+  			}	
+    		}
+		if(DEBUG) printf("fileLines-%d\n",fileLines);
+		if(DEBUG) printf("size is %d",size);
+		if (fileLines % (size-1) == 0) { 
+			if(DEBUG) printf("EQUAL");
+			linesPerNode = fileLines/(size-1);
+			linesLastNode= linesPerNode;
+			if(DEBUG) printf ("Lines and last: %d and %d\n",linesPerNode,linesLastNode);
+		}
+		else { 
+			if(DEBUG) printf("UNEQUAL");
+			linesPerNode = fileLines/(size-1);
+			linesLastNode = (fileLines - (linesPerNode * (size -2)));
+			if(DEBUG) printf ("Lines and last: %d and %d\n",linesPerNode,linesLastNode);
+		}
+
+		//rewind(fp);
+	
+
+		for (dest=1; dest < size; dest++) {
+			if (dest == (size -1) ) {
+				linesPerNode = linesLastNode;
+			}
+			MPI_Send(&linesPerNode,1,MPI_INT,dest,1,MPI_COMM_WORLD);
+			MPI_Send(&fileLines,1,MPI_INT,dest,1,MPI_COMM_WORLD);
+			if(DEBUG) printf("linesPerNode %d and rank %d", linesPerNode,dest);
+	
+		}
+		time1 = microtime();
+		for (dest=1; dest < size; dest++) {
+			MPI_Recv(&totalTime,1,MPI_DOUBLE,dest,1,MPI_COMM_WORLD,&status);
+			if(DEBUG) printf("NODE %d ran for %g us\t  Timer Resolution = %g us\t\n", dest,totalTime, get_microtime_resolution());
+			totalTime += totalTime;
+		}
+		time2 = microtime();
+		t = time2-time1;
+		printf("TOTAL RUNTIME = %g us\n", t);
+		printf("PROCESSING TIME = %g us\n",totalTime);
+		printf("COMMUNICATION TIME = %g us\n",t -totalTime); 
+	
+		fclose(fp);
+		MPI_Finalize();
+
+	}
+
+	if (rank > 0) {
+		double t, time1, time2, totalTime = 0.00;
+		FILE * fp = fopen("PASSWORDFILE.txt", "r");
+		char * line = NULL;
+  		size_t len = 0;
+		ssize_t read;
+		int endLine, startLine, curFileLine,linesPerNode;
+  	
+		MPI_Recv(&linesPerNode,1,MPI_INT,0,1,MPI_COMM_WORLD,&status);
+  		MPI_Recv(&fileLines,1,MPI_INT,0,1,MPI_COMM_WORLD,&status);
+		if(DEBUG) printf("RANK %d LINESPER %d FILESIZE %d\n",rank,linesPerNode,fileLines);
+
+		if (rank != (size -1)) { 
+			startLine = (linesPerNode * rank)-(linesPerNode -1);
+ 			endLine = startLine + linesPerNode;
+			if(DEBUG) printf("start: %d and end: %d\n",startLine, endLine);
+		}
+		else {
+			if(DEBUG) printf("LAST NODE:%d\t",rank);  
+			startLine = (fileLines - linesPerNode)+1;
+		
+ 			endLine = fileLines + 1; 
+			if(DEBUG) printf("start: %d and end: %d\n",startLine, endLine);
+		}
+		//curFileLine=0;
+		#pragma omp parallel 
+		{
+		#pragma omp parallel for schedule(static)
+		for (curFileLine = 0; curFileLine < endLine; curFileLine++) {
+		//while ((read = getline(&line, &len, fp)) != -1) {
+			//curFileLine++;
+			read = getline(&line, &len, fp);
+			if ((startLine <= curFileLine) && (curFileLine < endLine) && read != -1) {
+				time1 = microtime();
+				if(DEBUG) printf("start %d and curfileline %d and end line %d\n",startLine,curFileLine,endLine);
+ 				long length = strlen(line);
+				char solved = 0;
+				char *pw, *guess, *known;
+				int unknown = 0;
+
+				pw = init(length);
+				guess = init(length);
+				known = init(length);
+	
+				int i;
+				for(i = 0; i < length; i++) {
+					pw[i] = line[i];
+				}
+ 
+				while(!solved) {
+					guess = makeGuess(guess, known, length, &unknown);
+
+					solved = checkGuess(guess, pw, known, length, &unknown);
+				}
+				time2 = microtime();
+				if(DEBUG) printf("The password on rank %d is %s\t", rank,guess);
+
+				t = time2-time1;
+				if(DEBUG) printf("rank %d time %g\n",rank,t);
+				#pragma omp critical 
+				totalTime += t;
+
+				free(pw);
+				free(guess);
+				free(known);
+
+			}
+		}
+		}
+
+		MPI_Send(&totalTime,1,MPI_DOUBLE,0,1,MPI_COMM_WORLD);
+		
+		fclose(fp);
+		MPI_Finalize();
+
+		if (line)
+			free(line);
+	}
+	exit(EXIT_SUCCESS);
+	printf("HELP");
+	return 0;
+}
